ec1070f8123fe3dc031f1ab4e7d522fd38abf732
Ja_baka <nikolay.syatkovskiy@outlook.com>
2021-11-22 02:38:49 +0300

    Почистил скрипт кубика

--- Assets/BoardGame/Scripts/Dice.cs
+++ Assets/BoardGame/Scripts/Dice.cs
@@ -1,44 +1,15 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Dice : MonoBehaviour, IPointerClickHandler
 {
-	public int Value { get; private set; } = 0;
+	public EventHandler<DiceEventArgs> RolledEvent;
 
-	[SerializeField] private float _jumpForce = 10f;
-	
-	private DiceSide[] _sides = new DiceSide[6];
 	private Rigidbody _rigidbody;
-	private bool _isThrown = false;
-    private bool _isActualValue = false;
 
-	private void Start()
-	{
-		_rigidbody = GetComponent<Rigidbody>();
-		_sides = GetComponentsInChildren<DiceSide>();
-	}
-
-    private void FixedUpdate()
-    {
-		_isThrown = !_rigidbody.IsSleeping();
-        
-        if (_isThrown == false
-            && _isActualValue == false)
-        {
-			DiceSideCheck();
-
-			if (Value == 0)
-			{
-				Rolling();
-				return;
-			}
-
-			_isActualValue = true;
-            Debug.Log($"Value = {Value}");
-			
-        }
-    }
+	private const float _jumpForce = 10f;
 
 	public void OnPointerClick(PointerEventData eventData)
 	{
@@ -47,33 +18,44 @@ public class Dice : MonoBehaviour, IPointerClickHandler
 
 	private void Rolling()
 	{
-		if (_isThrown == true)
+		if (_rigidbody.IsSleeping() == false)
 		{
 			return;
 		}
 
-		_isActualValue = false;
-
 		_rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
 
-		float randomX = Random.Range(-360f, 360f);
-		float randomY = Random.Range(-360f, 360f);
-		float randomZ = Random.Range(-360f, 360f);
+		float randomX = UnityEngine.Random.Range(-360f, 360f);
+		float randomY = UnityEngine.Random.Range(-360f, 360f);
+		float randomZ = UnityEngine.Random.Range(-360f, 360f);
 
 		_rigidbody.AddTorque(randomX, randomY, randomZ);
 	}
 
+	private void Start()
+	{
+		_rigidbody = GetComponent<Rigidbody>();
+	}
+
+    private void FixedUpdate()
+    {
+        if (_rigidbody.IsSleeping() == true)
+        {
+			DiceSideCheck();
+        }
+    }
+
 	private void DiceSideCheck()
     {
-		Value = 0;
-		foreach (DiceSide side in _sides)
+		foreach (DiceSide side in GetComponentsInChildren<DiceSide>())
 		{
 			if (side.IsGrounded)
 			{
-				Value = side.SideValue;
-				Debug.Log($"выпало число {Value}");
+				RolledEvent?.Invoke(this, new DiceEventArgs(side.SideValue));
 				return;
 			}
 		}
-    }
+
+		Rolling();
+	}
 }
