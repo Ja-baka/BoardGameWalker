d012821723dc3cb442cbe8c344de855ac5bbe176
Ja-baka <73071447+Ja-baka@users.noreply.github.com>
2021-12-05 10:25:20 +0300

    Над кубиком отображается выпавшее значение

--- Assets/BoardGame/Scripts/UI/DiceValue.cs
+++ Assets/BoardGame/Scripts/UI/DiceValue.cs
@@ -1,8 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DiceValue : MonoBehaviour
 {
-    
+	private Dice _dice;
+	private TextMeshPro _text;
+
+	private void Awake()
+	{
+		_text = GetComponentInChildren<TextMeshPro>();
+		_dice = FindObjectOfType<Dice>();
+	}
+	
+	private void OnEnable()
+	{
+		_dice.ThrownEvent += Thrown;
+		_dice.RolledEvent += Rolled;
+	}
+
+
+	private void OnDisable()
+	{
+		if (_dice != null)
+		{
+			_dice.ThrownEvent -= Thrown;
+			_dice.RolledEvent -= Rolled;
+		}
+	}
+	private void Thrown(object sender, DiceEventArgs e)
+	{
+		_text.enabled = false;
+	}
+	private void Rolled(object sender, DiceEventArgs e)
+	{
+		_text.text = e.Value.ToString();
+		_text.enabled = true;
+	}
 }
